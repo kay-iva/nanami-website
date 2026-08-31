0f57ecbaa453f263f641851441a282f3e0d46ddf
Kay <mt241065@ustp-students.at>
2026-08-31 13:02:29 +0200

    calendar v1.1

--- src/lib/components/booking/BookingCalendar.svelte
+++ src/lib/components/booking/BookingCalendar.svelte
@@ -5,77 +5,47 @@
         'kaloyan-ivanov-d8shly/klavier-einzelstunde-30-min';
 
     onMount(() => {
-        (function (C: Window, A: string, L: string) {
-            const p = function (a: { q: unknown[] }, ar: IArguments) {
-                a.q.push(ar);
-            };
+        const script = document.createElement('script');
 
-            const d = C.document;
+        script.src = 'https://app.cal.com/embed/embed.js';
+        script.async = true;
 
-            C.Cal =
-                C.Cal ||
-                function (...args: unknown[]) {
-                    const cal = C.Cal as {
-                        loaded?: boolean;
-                        ns?: Record<string, unknown>;
-                        q?: unknown[];
-                    };
+        script.onload = () => {
+            const Cal = window.Cal;
 
-                    if (!cal.loaded) {
-                        cal.ns = {};
-                        cal.q = cal.q || [];
-
-                        const script = d.createElement('script');
-                        script.src = A;
-                        d.head.appendChild(script);
-
-                        cal.loaded = true;
-                    }
-
-                    if (args[0] === L) {
-                        const api = function (...apiArgs: unknown[]) {
-                            p(api as unknown as { q: unknown[] }, apiArgs as unknown as IArguments);
-                        };
-
-                        (api as unknown as { q: unknown[] }).q = [];
-
-                        (cal.ns as Record<string, unknown>)[L] = api;
-
-                        p(
-                            api as unknown as { q: unknown[] },
-                            args as unknown as IArguments
-                        );
-                    } else {
-                        p(
-                            cal as unknown as { q: unknown[] },
-                            args as unknown as IArguments
-                        );
-                    }
-                };
-        })(window, 'https://app.cal.com/embed/embed.js', 'booking');
-
-        const Cal = (
-            window as Window & {
-                Cal: (...args: unknown[]) => void;
+            if (!Cal) {
+                console.error('Cal.com embed could not be loaded.');
+                return;
             }
-        ).Cal;
 
-        Cal('init', 'booking', {
-            origin: 'https://cal.com'
-        });
+            Cal('init', 'booking', {
+                origin: 'https://cal.com'
+            });
 
-        Cal.ns.booking('inline', {
-            elementOrSelector: '#cal-booking',
-            calLink,
-            config: {
+            Cal('inline', {
+                elementOrSelector: '#cal-booking',
+                calLink,
+                config: {
+                    layout: 'month_view'
+                }
+            });
+
+            Cal('ui', {
+                styles: {
+                    branding: {
+                        brandColor: '#66756a'
+                    }
+                },
+                hideEventTypeDetails: false,
                 layout: 'month_view'
-            }
-        });
+            });
+        };
 
-        Cal.ns.booking('ui', {
-            hideEventTypeDetails: false,
-            layout: 'month_view'
-        });
+        document.head.appendChild(script);
+
+        return () => {
+            script.remove();
+        };
     });
 </script>
 
