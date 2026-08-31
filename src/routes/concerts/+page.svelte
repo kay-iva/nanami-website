063d91ac6e498031d884840c7accc71e6ca07e99
Kay <mt241065@ustp-students.at>
2026-08-31 12:05:12 +0200

    testing the server config

--- src/routes/concerts/+page.svelte
+++ src/routes/concerts/+page.svelte
@@ -163,6 +163,8 @@
                 </p>
             </div>
 
+            <pre>{JSON.stringify(data.galleryImages, null, 2)}</pre>
+
             {#if data.galleryImages.length > 0}
                 <PhotoGallery images={data.galleryImages} />
             {:else}
